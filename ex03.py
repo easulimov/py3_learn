7840d4c5bcb485c8cc3746bffb2a1c4d942d034f
Evgeniy <fox_su18@mail.ru>
2021-05-05 17:49:06 +0300

    exercise03

---
+++ ex03.py
@@ -0,0 +1,20 @@
+print('I will count the animals')
+print("Chicken", 25+30/6)
+print("Roosters", 100-25*3%4)
+
+print('And now I will count the eggs:')
+print(3+2+1-5+4%2-1/4+6)
+
+print('Is it true that 3+2<5-7')
+print(3+2<5-7)
+
+print("How much will 3+2", 3+2)
+print("And how much will 5-7?", 5-7)
+
+print("Oh I think I'm confused. Why False?")
+
+print("I will count again...")
+
+print('Is it greater?', 5>-2)
+print("And is it greater or equal?", 5>=-2)
+print("And is it less or equal?", 5<=-2)
