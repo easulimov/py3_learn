f69cac41c8478edb135800e876d1093f2b5e2bab
Evgeniy <fox_su18@mail.ru>
2021-05-05 17:49:24 +0300

    exercise05

---
+++ ex05.py
@@ -0,0 +1,19 @@
+my_name = 'Zed Show'
+my_age = 35 #It's true
+my_height = 188 #cm
+my_weight = 80 #kg
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'maroon'
+
+print(f"Let's talk about a man named {my_name}.")
+print(f"His height is {my_height}.")
+print(f'His weight is {my_weight}.')
+print(f"In fact it is not that much.")
+print(f"Hi has {my_eyes} eyes and {my_hair} my_hair.")
+print(f"Usually his teeth {my_teeth}, although he likes to drink coffee")
+
+#This this line of code is pretty tricky
+
+total = my_age + my_height + my_weight
+print(f"If I add up {my_age}, {my_height} and {my_weight} I will get {total}")
