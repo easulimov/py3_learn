f5af1fa3db81ebf31e7e0d0d6d5682e06b3900ea
Evgeniy <fox_su18@mail.ru>
2021-05-06 14:22:20 +0300

    Exercise05_with_practice

--- ex05.py
+++ ex05.py
@@ -1,19 +1,29 @@
-my_name = 'Zed Show'
-my_age = 35 #It's true
-my_height = 188 #cm
-my_weight = 80 #kg
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'maroon'
+name = 'Zed Show'
+age = 35 #It's true
+height = 188 #cm
+weight = 80 #kg
+eyes = 'Blue'
+teeth = 'White'
+hair = 'maroon'
 
-print(f"Let's talk about a man named {my_name}.")
-print(f"His height is {my_height}.")
-print(f'His weight is {my_weight}.')
+print(f"Let's talk about a man named {name}.")
+print(f"His height is {height}.")
+print(f'His weight is {weight}.')
 print(f"In fact it is not that much.")
-print(f"Hi has {my_eyes} eyes and {my_hair} hair.")
-print(f"Usually his teeth is {my_teeth}, although he likes to drink coffee")
+print(f"Hi has {eyes} eyes and {hair} hair.")
+print(f"Usually his teeth is {teeth}, although he likes to drink coffee")
 
 #This this line of code is pretty tricky
 
-total = my_age + my_height + my_weight
-print(f"If I add up {my_age}, {my_height} and {my_weight} I will get {total}")
+total = age + height + weight
+print(f"If I add up {age}, {height} and {weight} I will get {total}")
+
+
+centimeters = 10
+kilograms = 10
+
+meters = centimeters / 100
+tons = kilograms / 1000
+
+print(f'{centimeters} centimeters = {meters} meters')
+print(f'{kilograms} kilograms = {tons} tons')
