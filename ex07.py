30f3990c0aa1aea5104b35de7a08efd060803afe
Evgeniy.S <fox_su18@mail.ru>
2021-05-09 14:30:18 +0300

    exercise 07

---
+++ ex07.py
@@ -0,0 +1,19 @@
+print("Mary had a little lamb")
+print("His wool was white as {}.".format('snow'))
+print("And wherever Mary went")
+print("The little lamb always followed her")
+print(10 * ".") # What does it mean?
+
+
+end1 = "Б"
+end2 = "а"
+end3 = "д"
+end4 = "д"
+end5 = "и"
+end6 = "Г"
+end7 = "а"
+end8 = "й"
+
+# Pay attention to the first comma. Remove this comma. What happend?
+print(end1 + end2 + end3 + end4 + end5, end=" ")
+print(end6 + end7 + end8)
