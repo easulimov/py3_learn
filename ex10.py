5f76f1a2df5777b2054490216c976431188b592f
Evgeniy.S <fox_su18@mail.ru>
2021-05-11 13:46:56 +0300

    exercise 10

---
+++ ex10.py
@@ -0,0 +1,17 @@
+d_artagnan = "\tMy name is D'Artagnan"
+athos = "this line\nis split in two"
+porthos = "I am \\ - \\ a musketeer"
+
+aramis= """
+Hints:
+\t* Count of Gascony
+\t* Tied up with Milady
+\t* Baron\n\t* General of the Order
+"""
+
+
+print(d_artagnan)
+print(athos)
+print(porthos)
+print(aramis)
+
