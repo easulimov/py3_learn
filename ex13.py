f30836f0f01441d8a5351cef7683e505dd71553c
Evgeniy <fox_su18@mail.ru>
2021-10-10 11:33:10 +0300

    exercise12

---
+++ ex13.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+script, first, second, third= argv
+print("Этот сценарий называется: ", script)
+print("Моя первая переменная назывется ", first)
+print("Моя первая переменная назывется ", second)
+print("Моя первая переменная назывется ", third)
+new_str=input("Напишите еще что нибудь =) :")
+print(new_str)
